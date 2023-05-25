6932402b6d29df06d3a03fc373981e2e2e621421
MartinaRecalde <mrecalde@frba.utn.edu.ar>
2023-05-24 23:33:09 -0300

    esquema de carrusel

---
+++ carrusel.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const grande    = document.querySelector('.grande')
+const punto     = document.querySelectorAll('.punto')
+
+punto.forEach( ( cadaPunto , i )=> {
+    punto[i].addEventListener('click',()=>{
+
+        let posicion  = i
+        let operacion = posicion * - ( 100 / 6)
+        grande.style.transform = `translateX(${ operacion }%)`
+        punto.forEach( ( cadaPunto , i )=>{
+            punto[i].classList.remove('activo')
+        })
+        punto[i].classList.add('activo')
+
+    })
+})
